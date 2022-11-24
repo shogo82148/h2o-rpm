b18ce4e1bc20b388d19c96953f2e6403fb8c3ecd
ICHINOSE Shogo <shogo82148@gmail.com>
2022-11-25 04:26:48 +0900

    fix the test

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,9 +7,27 @@ IMAGE=$(perl -ne 'print $1 if /FROM\s+(.*)/' "Dockerfile.$H2O_DISTRO")
 ROOT=$(cd "$(dirname "$0")/../" && pwd)
 : "${PLATFORM:=linux/amd64}"
 
-docker run \
-    --rm \
-    -v "$ROOT/$H2O_DISTRO.build:/build" \
-    --platform "$PLATFORM" \
-    "$IMAGE" \
-    sh -c "if command -v dnf; then dnf update -y && dnf --enablerepo powertools install -y \"/build/RPMS/\$(uname -m)/\"*.rpm; else yum update -y; yum install -y epel-release; yum install -y \"/build/RPMS/\$(uname -m)/\"*.rpm; fi"
+if [[ "$H2O_DISTRO" = amazonlinux2022 ]]; then
+    docker run \
+        --rm \
+        -v "$ROOT/$H2O_DISTRO.build:/build" \
+        --platform "$PLATFORM" \
+        "$IMAGE" \
+        sh -c "dnf update -y && dnf install -y \"/build/RPMS/\$(uname -m)/\"*.rpm"
+
+elif docker run --rm --platform "$PLATFORM" "$IMAGE" sh -c "command -v dnf"; then
+    docker run \
+        --rm \
+        -v "$ROOT/$H2O_DISTRO.build:/build" \
+        --platform "$PLATFORM" \
+        "$IMAGE" \
+        sh -c "dnf update -y && dnf --enablerepo powertools install -y \"/build/RPMS/\$(uname -m)/\"*.rpm"
+
+else 
+    docker run \
+        --rm \
+        -v "$ROOT/$H2O_DISTRO.build:/build" \
+        --platform "$PLATFORM" \
+        "$IMAGE" \
+        sh -c "yum update -y; yum install -y epel-release; yum install -y \"/build/RPMS/\$(uname -m)/\"*.rpm"
+fi
